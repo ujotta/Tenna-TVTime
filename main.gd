aebca91dcb2db6fb5cef0cea44d85c0b685bb488
ujotta <jpaulofernands@gmail.com>
2025-07-21 21:30:12 -0300

    initial commit

---
+++ main.gd
@@ -0,0 +1,46 @@
+extends Node
+
+@onready var faces: AnimatedSprite2D = $Faces
+@onready var timer: Timer = $Timer
+@onready var talk: AudioStreamPlayer = $Talk
+
+var time := .3
+var rot := 0.0
+var rot2 := 0.0
+@export var rotmax = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_custom_mouse_cursor(preload("res://img/UI/cursor2.png"))
+	timer.timeout.connect(_on_timer_timeout)
+	faces.play()
+	talk.play()
+	talk.stream_paused = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rot += 1 * delta
+	rot2 += 2 * delta
+	faces.position.x = 200 + Vector2(10, 0).rotated(rot).x
+	faces.position.y = 150 + Vector2(5, 0).rotated(rot2).y
+	faces.rotation = deg_to_rad(Vector2(rotmax, 0).rotated(rot).x)
+	if Input.is_action_just_pressed("ui_up"):
+		print("press")
+		#talk.playing = true
+		talk.stream_paused = false
+		#timer.wait_time += time
+		if timer.is_stopped():
+			timer.start()
+			timer.wait_time = time
+	elif Input.is_action_pressed("ui_down"):
+		talk.stream_paused = false
+	elif timer.is_stopped():
+		talk.stream_paused = true
+	faces.speed_scale = 0
+	if !talk.stream_paused:
+		faces.speed_scale = 1
+	
+func _on_timer_timeout() -> void:
+	talk.stream_paused = true
+	print("stop")
---
+++ scripts/intro.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var timer := false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_custom_mouse_cursor(preload("res://img/UI/cursor2.png"))
+	$Control/AnimatedSprite2D.position.x = int(get_viewport().get_visible_rect().size.x /2)
+	$Control/ColorRect2.material.set_shader_parameter("progress", -.25)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if timer && !$Control/ColorRect2.material.get_shader_parameter("progress") >= 1.1:
+		$Control/ColorRect2.material.set_shader_parameter("progress",$Control/ColorRect2.material.get_shader_parameter("progress") + delta / 4)
+	elif $Control/ColorRect2.material.get_shader_parameter("progress") >= 1.1:
+		get_tree().change_scene_to_file("res://main.tscn")
+
+func start() -> void:
+	timer = true
